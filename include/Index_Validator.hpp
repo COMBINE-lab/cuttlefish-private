6dbadc178b802f1425b45a4e5e66450ac4f3a297
jamshed <jamshed@umd.edu>
2022-07-12 16:24:53 -0400

    Cross-check and validate index

--- include/Index_Validator.hpp
+++ include/Index_Validator.hpp
@@ -68,6 +68,76 @@ inline bool Kmer_Index<k>::validate(const std::string& file_path)
 
     kmer_index.index();
 
+    std::cout << "Constructed the naive and the Cuttlefish index.\n";
+
+    std::cout <<    "\n\nCross-checking the indices.\n"
+                    "===============================\n\n";
+
+    // Check paths' validity.
+
+    std::cout << "Path counts:\n\tNaive idx: " << ends.size() << ", Cuttlefish idx: " << kmer_index.path_count << ".\n";
+    if(ends.size() != kmer_index.path_count)
+        return false;
+
+    for(std::size_t i = 0; i < ends.size(); ++i)
+        if(ends[i] != kmer_index.path_ends->at(i))
+            return false;
+
+    std::cout << "Path endpoint indices matched.\n";
+
+    std::cout << "Path sequence lengths:\n\tNaive idx: " << paths.size() << ", Cuttlefish idx: " << kmer_index.sum_paths_len << ".\n";
+    if(paths.size() != kmer_index.sum_paths_len)
+        return false;
+
+    for(std::size_t i = 0; i < paths.size(); ++i)
+        if(paths[i] != DNA_Utility::map_char(DNA::Base(kmer_index.paths[i].get())))
+            return false;
+
+    std::cout << "Path sequences aligned.\n";
+
+
+    // Check minimizer instance counts and their offsets in the paths.
+
+    std::cout << "Unique minimizer count:\n\tNaive idx: " << M.size() << ", Cuttlefish idx: " << kmer_index.min_count << ".\n";
+    std::cout << "Minimizer instance count:\n\tNaive idx: " << inst_count << ", Cuttlefish idx: " << kmer_index.num_instances << ".\n";
+    if(M.size() != kmer_index.min_count || inst_count != kmer_index.num_instances)
+        return false;
+
+    const auto& mi_count = *kmer_index.min_instance_count;
+    const auto& m_offset = *kmer_index.min_offset;
+    for(const auto p : M)
+    {
+        const cuttlefish::minimizer_t min = p.first;
+        const uint64_t hash = kmer_index.hash(min) - 1;
+        if(hash >= M.size())
+        {
+            std::cout << "Alien minimizer encountered.\n";
+            return false;
+        }
+
+        const std::size_t count = mi_count[hash] - (hash > 0 ? mi_count[hash - 1] : 0);
+        if(count != p.second.size())
+        {
+            std::cout << "Instance count for minimizer: " << Kmer<l>(p.first) << " with hash " << hash << ":\n\t"
+                            "Naive idx: " << p.second.size() << ", Cuttlefish idx: " << count << "\n";
+            std::cout << "Instance counts don't match for some minimizers.\n";
+            return false;
+        }
+
+
+        const std::size_t min_blk_off = (hash > 0 ? mi_count[hash - 1] : 0);
+        for(std::size_t i = 0; i < count; ++i)
+            if(m_offset[min_blk_off + i] != p.second[i])
+            {
+                std::cout << "Differing instance offset for minimizer: " << Kmer<l>(p.first) << " with hash " << hash << ":\n\t"
+                                "Naive idx: " << p.second[i] << ", Cuttlefish idx: " << m_offset[min_blk_off + i] << "\n";
+                std::cout << "Instance offsets don't match for some minimizers.\n";
+                return false;
+            }
+    }
+
+    std::cout << "Instance count and offsets of individual minimizers matched.\n";
+
 
     return true;
 }
