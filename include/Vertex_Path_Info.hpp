53cbc7f748f8be022202e247c6fa4f1fc52ee7e5
jamshed <jamshed@umd.edu>
2023-06-22 22:44:54 -0400

    Refactor vertex path-info

--- include/Vertex_Path_Info.hpp
+++ include/Vertex_Path_Info.hpp
@@ -26,31 +26,32 @@ public:
 
 private:
 
-    const path_id_t p_v_;   // The path-ID of the vertex.
-    const weight_t r_v_;    // The rank of the vertex in the path.
-    const side_t o_v_;  // The orientation of the vertex in its specified rank—the path traversal exits `v` through the side `o_v`.
+    const path_id_t p_; // The path-ID of the vertex.
+    const weight_t r_;  // The rank of the vertex in the path.
+    const side_t o_;    // The orientation of the vertex in its specified rank—the path traversal exits `v` through the side `o_v`.
 
 
 public:
 
-    // Constructs a path-info object for a k-mer such that its path-ID is `p_v`
-    // and rank in the path is `r_v` when the path is traversed in the
-    // orientation such that the traversal exits `v` through its side `o_v`.
-    Vertex_Path_Info(const path_id_t p_v, const weight_t r_v, const side_t o_v):
-          p_v_(p_v)
-        , r_v_(r_v)
-        , o_v_(o_v)
+    // Constructs a path-info object for a vertex such that its path-ID is `p`
+    // and rank in the path is `r` when the path is traversed in the
+    // orientation such that the traversal exits the vertex through its side
+    // `o`.
+    Vertex_Path_Info(const path_id_t p, const weight_t r, const side_t o):
+          p_(p)
+        , r_(r)
+        , o_(o)
     {}
 
     // Returns the path-ID of the vertex.
-    const path_id_t p_v() const { return p_v_; }
+    const path_id_t p() const { return p_; }
 
     // Returns the rank of the vertex in the path.
-    weight_t r_v() const { return r_v_; }
+    weight_t r() const { return r_; }
 
     // Returns the orientation `o_v` of the vertex in its specified rank—the
     // path traversal exits the vertex through the side `o_v`.
-    side_t o_v() const { return o_v_; }
+    side_t o() const { return o_; }
 };
 
 }
