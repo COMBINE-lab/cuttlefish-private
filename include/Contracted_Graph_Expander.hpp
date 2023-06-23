e58f332c725895fa5dfde45c4ed1c531fa2e216e
jamshed <jamshed@umd.edu>
2023-06-23 17:11:34 -0400

    Update expander fields

--- include/Contracted_Graph_Expander.hpp
+++ include/Contracted_Graph_Expander.hpp
@@ -5,9 +5,12 @@
 
 
 #include "Edge_Matrix.hpp"
+#include "Vertex_Path_Info.hpp"
+#include "Ext_Mem_Bucket.hpp"
 
 #include <cstdint>
 #include <cstddef>
+#include <vector>
 #include <string>
 
 
@@ -23,6 +26,8 @@ private:
 
     Edge_Matrix<k>& E;  // Edge matrix of the (augmented) discontinuity graph.
 
+    std::vector<Ext_Mem_Bucket<Vertex_Path_Info_Pair<k>>>& P_v; // `P_v[i]` contains path-info for vertices in partition `i`.
+
     const std::string work_path;    // Path-prefix to temporary working files.
 
 
@@ -30,7 +35,7 @@ public:
 
     // Constructs an expander for the contracted discontinuity-graph with edge-
     // matrix `E`. Temporary files are stored at path-prefix `temp_path`.
-    Contracted_Graph_Expander(Edge_Matrix<k>& E, const std::string& temp_path);
+    Contracted_Graph_Expander(Edge_Matrix<k>& E, std::vector<Ext_Mem_Bucket<Vertex_Path_Info_Pair<k>>>& P_v, const std::string& temp_path);
 };
 
 }
--- src/Contracted_Graph_Expander.cpp
+++ src/Contracted_Graph_Expander.cpp
@@ -6,8 +6,9 @@ namespace cuttlefish
 {
 
 template <uint16_t k>
-Contracted_Graph_Expander<k>::Contracted_Graph_Expander(Edge_Matrix<k>& E, const std::string& temp_path):
+Contracted_Graph_Expander<k>::Contracted_Graph_Expander(Edge_Matrix<k>& E, std::vector<Ext_Mem_Bucket<Vertex_Path_Info_Pair<k>>>& P_v, const std::string& temp_path):
       E(E)
+    , P_v(P_v)
     , work_path(temp_path)
 {}
 
