a9645accdcf03a7dff60fcda06f40dc7aa047973
jamshed <jamshed@umd.edu>
2023-07-04 15:34:02 -0400

    Use lengths instead of offsets in unitig-files

--- include/Unitig_File.hpp
+++ include/Unitig_File.hpp
@@ -17,41 +17,49 @@ namespace cuttlefish
 // Unitig-file writer manager.
 class Unitig_File_Writer
 {
+    typedef uint16_t uni_len_t; // Type of the length of a unitig in a bucket.
+
 private:
 
     static constexpr std::size_t in_memory_bytes = 16lu * 1024; // 16 KB.
-    static constexpr auto in_memory_off = in_memory_bytes / sizeof(std::size_t);
+    static constexpr auto in_memory_len = in_memory_bytes / sizeof(uni_len_t);
 
     const std::string file_path;    // Path to the file for the unitig content.
 
     std::vector<char> buf;  // In-memory buffer for the unitig content.
 
     std::size_t total_sz;   // Total size of the added unitig content.
-    std::vector<std::size_t> offset;    // Offsets of the unitigs into the file.
-    std::size_t unitig_c;   // Number of unitigs.
+    std::vector<uni_len_t> len; // Lengths of the unitigs in the file.
+    std::size_t unitig_c;   // Number of unitigs added.
     std::ofstream output; // The unitig file.
-    std::ofstream output_off;   // The offsets file.
+    std::ofstream output_len;   // The lengths file.
 
 
-    // Returns path to the file containing the offsets of the unitigs in their
-    // concatenated file.
-    const std::string offset_file_path() const { return file_path + std::string(".off"); }
+    // Returns path to the file containing the lengths of the unitigs.
+    const std::string length_file_path() const { return file_path + std::string(".len"); }
 
     // Flushes the in-memory unitig content to external memory.
     void flush_unitigs();
 
-    // Flushes the in-memory unitig offsets to external memory.
-    void flush_offsets();
+    // Flushes the in-memory unitig lengths to external memory.
+    void flush_lengths();
 
 
 public:
 
+    // Constructs a unitig-writer to the file at path `file_path`.
     Unitig_File_Writer(const std::string& file_path);
 
+    // Returns the total size of the added unitig content.
+    auto size() const { return total_sz; }
+
+    // Returns the number of unitigs added.
     auto unitig_count() const { return unitig_c; }
 
+    // Adds the unitig content in the range `[beg, end)` to the writer.
     template <typename T_it_> void add(T_it_ beg, T_it_ end);
 
+    // Closes the stream.
     void close();
 };
 
@@ -59,7 +67,7 @@ public:
 template <typename T_it_>
 inline void Unitig_File_Writer::add(const T_it_ beg, const T_it_ end)
 {
-    offset.push_back(total_sz);
+    len.push_back(end - beg);
     buf.insert(buf.end(), beg, end);
     total_sz += (end - beg);
     unitig_c++;
@@ -67,22 +75,22 @@ inline void Unitig_File_Writer::add(const T_it_ beg, const T_it_ end)
     if(buf.size() >= in_memory_bytes)
         flush_unitigs();
 
-    if(offset.size() >= in_memory_off)
-        flush_offsets();
+    if(len.size() >= in_memory_len)
+        flush_lengths();
 }
 
 
 inline void Unitig_File_Writer::flush_unitigs()
 {
-    output.write(reinterpret_cast<const char*>(buf.data()), buf.size() * sizeof(char));
+    output.write(reinterpret_cast<const char*>(buf.data()), buf.size() * sizeof(decltype(buf)::value_type));
     buf.clear();
 }
 
 
-inline void Unitig_File_Writer::flush_offsets()
+inline void Unitig_File_Writer::flush_lengths()
 {
-    output_off.write(reinterpret_cast<const char*>(offset.data()), offset.size() * sizeof(decltype(offset)::value_type));
-    offset.clear();
+    output_len.write(reinterpret_cast<const char*>(len.data()), len.size() * sizeof(decltype(len)::value_type));
+    len.clear();
 }
 
 }
--- src/Unitig_File.cpp
+++ src/Unitig_File.cpp
@@ -10,7 +10,7 @@ Unitig_File_Writer::Unitig_File_Writer(const std::string& file_path):
     , total_sz(0)
     , unitig_c(0)
     , output(file_path)
-    , output_off(offset_file_path())
+    , output_len(length_file_path())
 {}
 
 
@@ -19,11 +19,11 @@ void Unitig_File_Writer::close()
     if(!buf.empty())
         flush_unitigs();
 
-    if(!offset.empty())
-        flush_offsets();
+    if(!len.empty())
+        flush_lengths();
 
     output.close();
-    output_off.close();
+    output_len.close();
 }
 
 }
