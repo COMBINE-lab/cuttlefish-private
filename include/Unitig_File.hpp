ab5f8e243903f7333151767c66e5f05a84da21d4
jamshed <jamshed@umd.edu>
2023-07-04 17:25:51 -0400

    Add unitig reader

--- include/Unitig_File.hpp
+++ include/Unitig_File.hpp
@@ -4,10 +4,15 @@
 
 
 
+#include "Virtual_File.hpp"
+
 #include <cstddef>
+#include <limits>
+#include <cstring>
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cassert>
 
 
 namespace cuttlefish
@@ -17,7 +22,9 @@ namespace cuttlefish
 // Unitig-file writer manager.
 class Unitig_File_Writer
 {
-    typedef uint16_t uni_len_t; // Type of the length of a unitig in a bucket.
+    // TODO: use `u16` after testing done with `u32`.
+    // typedef uint16_t uni_len_t; // Type of the length of a unitig in a bucket.
+    typedef uint32_t uni_len_t; // Type of the length of a unitig in a bucket.
 
 private:
 
@@ -64,6 +71,54 @@ public:
 };
 
 
+// =============================================================================
+// Unitig-file reader manager.
+class Unitig_File_Reader
+{
+    typedef uint32_t uni_idx_t; // Type of the index of a unitig in a bucket.
+    // TODO: use `u16` after testing done with `u32`.
+    // typedef uint16_t uni_len_t; // Type of the length of a unitig in a bucket.
+    typedef uint32_t uni_len_t; // Type of the length of a unitig in a bucket.
+
+private:
+
+    static constexpr std::streamsize in_memory_bytes = 16lu * 1024; // 16 KB.
+
+    const std::string file_path;    // Path to the file with the unitig content.
+
+    std::vector<char> buf;  // In-memory buffer for the unitig content.
+    std::vector<uni_len_t> uni_len; // Sizes of the unitigs in the current buffer.
+
+    std::ifstream input;    // The unitigs-file.
+    Virtual_File<uni_len_t> len;    // The lengths-file.
+
+    std::size_t buf_idx;    // Index into the unitig-buffer for the next unitig to read-in.
+    uni_idx_t uni_idx_in_file;  // Index of the next unitig to read from file.
+    uni_idx_t uni_idx_in_mem;   // Index of the next unitig to parse from buffer.
+
+    const uni_idx_t unitig_count_;  // Number of unitigs in the file.
+    uni_idx_t unitig_parsed_;   // Number of unitigs parsed.
+    std::size_t total_sz;   // Total size of the read unitig content.
+
+
+    // Returns path to the file containing the lengths of the unitigs.
+    const std::string length_file_path() const { return file_path + std::string(".len"); }
+
+
+public:
+
+    // Constructs a unitig-reader for the file at path `file_path`.
+    Unitig_File_Reader(const std::string& file_path);
+
+    // Returns the number of unitigs in the file.
+    auto unitig_count() const { return unitig_count_; }
+
+    // Reads the next unitig into `unitig`; returns `true` iff there were
+    // unitigs remaining to be read.
+    template <typename T_> bool read_next_unitig(T_& unitig);
+};
+
+
 template <typename T_it_>
 inline void Unitig_File_Writer::add(const T_it_ beg, const T_it_ end)
 {
@@ -92,6 +147,51 @@ inline void Unitig_File_Writer::flush_lengths()
 {
     output_len.write(reinterpret_cast<const char*>(len.data()), len.size() * sizeof(decltype(len)::value_type));
     len.clear();
+}
+
+
+template <typename T_>
+inline bool Unitig_File_Reader::read_next_unitig(T_& unitig)
+{
+    if(buf_idx == buf.size())   // Buffer has been parsed completely; try a re-read.
+    {
+        assert(uni_idx_in_mem == uni_len.size());
+
+        if(uni_idx_in_file == unitig_count_)    // All unitigs have been read-off.
+            return false;
+
+        std::streamsize bytes_to_read = 0;
+        uni_len.clear();
+
+        while(uni_idx_in_file < unitig_count_ && bytes_to_read < in_memory_bytes)
+        {
+            uni_len.push_back(len[uni_idx_in_file]);
+            bytes_to_read += uni_len.back();
+
+            uni_idx_in_file++;
+        }
+
+        assert(bytes_to_read > 0);
+        buf.resize(bytes_to_read);
+        input.read(buf.data(), bytes_to_read * sizeof(char));
+        assert(input.gcount() == bytes_to_read);
+
+        buf_idx = 0;
+        uni_idx_in_mem = 0;
+    }
+
+
+    const auto len = uni_len[uni_idx_in_mem];
+    unitig.resize(len);
+    std::memcpy(unitig.data(), buf.data() + buf_idx, len * sizeof(decltype(buf)::value_type));
+
+    buf_idx += len;
+    uni_idx_in_mem++;
+    unitig_parsed_++;
+    total_sz += len;
+    assert(buf_idx <= buf.size());
+
+    return true;
 }
 
 }
--- src/Unitig_File.cpp
+++ src/Unitig_File.cpp
@@ -26,4 +26,17 @@ void Unitig_File_Writer::close()
     output_len.close();
 }
 
+
+Unitig_File_Reader::Unitig_File_Reader(const std::string& file_path):
+      file_path(file_path)
+    , input(file_path)
+    , len(length_file_path().c_str())
+    , buf_idx(0)
+    , uni_idx_in_file(0)
+    , uni_idx_in_mem(0)
+    , unitig_count_(len.size())
+    , unitig_parsed_(0)
+    , total_sz(0)
+{}
+
 }
