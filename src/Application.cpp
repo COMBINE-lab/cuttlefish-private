d9d43108b1ad4af46fbc0c7a15cea4db7b674701
jamshed <jamshed@umd.edu>
2023-11-22 14:44:44 -0500

    Make executable lighter
    no more provision for `k = 1`

--- src/Application.cpp
+++ src/Application.cpp
@@ -63,55 +63,6 @@ bool Application<k, T_App>::validate() const
 }
 
 
-template <template<uint16_t> typename T_App>
-Application<1, T_App>::Application(const Build_Params& params):
-    app(params.k() == 1 ? new T_App<1>(params) : nullptr),
-    validator(nullptr)
-{}
-
-
-template <template<uint16_t> typename T_App>
-Application<1, T_App>::Application(const Validation_Params& params):
-    app(nullptr),
-    validator(params.k() == 1 ? new Validator<1>(params) : nullptr)
-{}
-
-
-template <template<uint16_t> typename T_App>
-Application<1, T_App>::~Application()
-{
-    if(app != nullptr)
-        delete app;
-
-    if(validator != nullptr)
-        delete validator;
-}
-
-
-template <template<uint16_t> typename T_App>
-void Application<1, T_App>::execute() const
-{
-    if(app != nullptr)
-        app->construct();
-    else
-    {
-        std::cerr << "The provided k is not valid. Aborting.\n";
-        std::exit(EXIT_FAILURE);
-    }
-}
-
-
-template <template<uint16_t> typename T_App>
-bool Application<1, T_App>::validate() const
-{
-    if(validator != nullptr)
-        return validator->validate();
-
-    std::cerr << "The provided k is not valid. Aborting.\n";
-    std::exit(EXIT_FAILURE);
-}
-
-
 
 // Template instantiations for the required instances.
 template class Application<cuttlefish::MAX_K, CdBG>;
