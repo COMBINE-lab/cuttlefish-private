d04c3e97f424d2c59d56ad7acf82d43ae6ef415f
jamshed <jamshed@umd.edu>
2024-06-30 22:39:42 -0400

    Remove redundant DS

--- include/Edge_Matrix.hpp
+++ include/Edge_Matrix.hpp
@@ -31,7 +31,6 @@ private:
     const std::size_t vertex_part_count_;   // Number of vertex-partitions in the graph; it needs to be a power of 2.
     const std::string path; // File-path prefix to the external-memory blocks of the matrix.
     std::vector<std::vector<Ext_Mem_Bucket_Concurrent<Discontinuity_Edge<k>>>> edge_matrix; // Blocked edge matrix.
-    std::vector<std::vector<Spin_Lock>> lock;   // Locks for the blocks for concurrent updates.
 
     mutable std::vector<std::size_t> row_to_read;   // `j`'th entry contains the row of the next block to read from column `j`.
     mutable std::vector<std::size_t> col_to_read;   // `i`'th entry contains the column of the next block to read from row `i`.
--- src/Edge_Matrix.cpp
+++ src/Edge_Matrix.cpp
@@ -26,15 +26,10 @@ Edge_Matrix<k>::Edge_Matrix(std::size_t part_count, const std::string& path):
     }
 
     edge_matrix.resize(part_count + 1);
-    lock.resize(part_count + 1);
     for(std::size_t i = 0; i <= part_count; ++i)
-    {
         for(std::size_t j = 0; j <= part_count; ++j)
             j < i ? edge_matrix[i].emplace_back() :
                     edge_matrix[i].emplace_back(bucket_file_path(i, j));
-
-        std::vector<Spin_Lock>(part_count + 1).swap(lock[i]);
-    }
 
     for(std::size_t i = 1; i <= part_count; ++i)
         col_to_read[i] = i + 1;
