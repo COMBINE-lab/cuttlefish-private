efbe2b6355a9a17599e9970ee6d7c13ef645e7f2
jamshed <jamshed@umd.edu>
2024-11-20 15:57:13 -0500

    Use XOR-combine

--- include/Source_Hash.hpp
+++ include/Source_Hash.hpp
@@ -26,7 +26,8 @@ inline uint64_t source_hash(const uint32_t source)
 inline uint64_t hash_combine(const uint64_t h_0, const uint64_t h_1)
 {
     // Ref: https://www.boost.org/doc/libs/1_46_1/doc/html/hash/reference.html#boost.hash_combine
-    return h_0 ^ (h_1 + 0x9e3779b9 + (h_0 << 6) + (h_0 >> 2));
+    // return h_0 ^ (h_1 + 0x9e3779b9 + (h_0 << 6) + (h_0 >> 2));
+    return h_0 ^ h_1;
 }
 
 }
